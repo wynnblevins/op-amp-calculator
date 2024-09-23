b67a953e20cee32eca4422dd6fb8530cc8340278
Wynn Blevins <wynnblevins@gmail.com>
2024-09-23 18:57:43 -0400

    initial commit, calculator works but need styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,248 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import './App.css';
+import { ToggleButton, ToggleButtonGroup, TextField, createTheme, ThemeProvider, Select, MenuItem, SelectChangeEvent } from '@mui/material';
+import { OpAmpSchematic } from './components';
+import styled from "@emotion/styled";
+
+export enum ResistanceUnit {
+  OHMS = "Ω",
+  KILOOHMS = "kΩ",
+  MEGAOHMS = "MΩ",
+  GIGAOHMS = "GΩ"
+};
+
+export enum VoltageUnit {
+  MILLIVOLTS = "mV",
+  VOLTS = "V",
+  KILOVOLTS = "kV",
+  MEGAVOLTS = "MV",
+  GIGAVOLTS = "GV"
+};
+
+const theme = createTheme({
+  components: {
+    // Name of the component
+    MuiTextField: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          width: '80%',
+          marginTop: "2vh"
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          width: '20%',
+          marginTop: "2vh"
+        },
+      },
+    },
+  },
+});
 
 function App() {
+  
+  const [state, setState] = useState({
+    inverting: false,
+    inputResistorValue: 0,
+    inputResistorUnit: ResistanceUnit.OHMS,
+    feedbackResistorValue: 0,
+    feedbackResistorUnit: ResistanceUnit.OHMS,
+    inputVoltage: 0,
+    inputVoltageUnit: VoltageUnit.VOLTS,
+    outputVoltage: 0,
+    gain: 0
+  });
+  
+  useEffect(() => {
+    performCalculations();
+  }, [JSON.stringify(state)]);
+
+  const onAmpTypeChange = async (e: any) => {
+    await setState({
+      ...state,
+      inverting: e.target.innerText === "INVERTING" ? true : false      
+    });
+  };
+
+  const onInputResistorChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputResistorValue = +e.target.value;
+    
+    await setState({
+      ...state,
+      inputResistorValue: inputResistorValue      
+    })
+  };
+
+  const onInputResistorUnitChange = async (e: SelectChangeEvent<string>) => {
+    const inputResistorUnit = e.target.value;
+    
+    await setState({
+      ...state,
+      // @ts-ignore
+      inputResistorUnit: inputResistorUnit   
+    });
+  };
+
+  const onFeedbackResistorChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const feedbackResistorValue = +e.target.value;
+    
+    await setState({
+      ...state,
+      feedbackResistorValue: feedbackResistorValue
+    });
+  };
+
+  const onFeedbackResistorUnitChange = async (e: SelectChangeEvent<string>) => {
+    const feedbackResistorUnit = e.target.value;
+    
+    await setState({
+      ...state,
+      // @ts-ignore
+      feedbackResistorUnit: feedbackResistorUnit      
+    });
+  };
+
+  const onInputVoltageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputVoltage = +e.target.value;
+    
+    await setState({
+      ...state,
+      inputVoltage: inputVoltage
+    });
+  };
+
+  const onInputVoltageUnitChange = async (e: SelectChangeEvent<string>) => {
+    const inputVoltageUnit = e.target.value;
+    
+    setState({
+      ...state,
+      // @ts-ignore
+      inputVoltageUnit: inputVoltageUnit
+    });
+  };
+
+  const determineResistanceInOhms = (value: number, unit: string): number => {
+    let KILO = 1000;
+    let MEGA = 1000000;
+    let GIGA = 1000000000;
+    let resistanceInOhms: number = 0;
+
+    switch (unit) {
+      case "Ω": 
+        resistanceInOhms = value;
+        break;
+      case "kΩ":
+        resistanceInOhms = value * KILO;
+        break;
+      case "MΩ":
+        resistanceInOhms = value * MEGA;
+        break;
+      case "GΩ":
+        resistanceInOhms = value * GIGA;
+        break;
+      default:
+        throw new Error("Unknown resistance unit encountered!");
+    }
+    
+    return resistanceInOhms;
+  };
+
+  const performCalculations = async () => {
+    const inputInOhms = determineResistanceInOhms(state.inputResistorValue, state.inputResistorUnit);
+    const feedbackInOhms = determineResistanceInOhms(state.feedbackResistorValue, state.feedbackResistorUnit);
+
+    let gain, output;
+    
+    if (state.inverting) {
+      gain = -(feedbackInOhms / inputInOhms);
+    } else {
+      gain = 1 + (feedbackInOhms / inputInOhms);
+    }
+
+    output = gain * state.inputVoltage;
+    
+    setState({
+      ...state,
+      gain,
+      outputVoltage: output
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Op Amp Calculator</h1>
+
+        <div className='schematicWrapper'>
+          <ToggleButtonGroup
+            color="primary"
+            value={state.inverting}
+            exclusive
+            onChange={onAmpTypeChange}
+            aria-label="Platform"
+          >
+            <ToggleButton value="Inverting">Inverting</ToggleButton>
+            <ToggleButton value="Non Inverting">Non-Inverting</ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+
+        <TextField type="number" label="Input Resistor" onChange={onInputResistorChange}></TextField>
+        <Select
+          id="inputResistorUnitSelect"
+          value={state.inputResistorUnit}
+          label="Input Resistor Unit"
+          onChange={onInputResistorUnitChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MenuItem value={'Ω'}>Ω</MenuItem>
+          <MenuItem value={'kΩ'}>kΩ</MenuItem>
+          <MenuItem value={'MΩ'}>MΩ</MenuItem>
+          <MenuItem value={'GΩ'}>GΩ</MenuItem>
+        </Select>
+        
+        <TextField type="number" label="Feedback Resistor" onChange={onFeedbackResistorChange}></TextField>
+        <Select
+          id="feedbackResistorUnitSelect"
+          value={state.feedbackResistorUnit}
+          label="Feedback Resistor Unit"
+          onChange={onFeedbackResistorUnitChange}
+        >
+          <MenuItem value={'Ω'}>Ω</MenuItem>
+          <MenuItem value={'kΩ'}>kΩ</MenuItem>
+          <MenuItem value={'MΩ'}>MΩ</MenuItem>
+          <MenuItem value={'GΩ'}>GΩ</MenuItem>
+        </Select>
+        
+        <TextField type="number" label="Input Voltage" onChange={onInputVoltageChange}></TextField>
+        <Select
+          id="inputVoltageUnitSelect"
+          value={state.inputVoltageUnit}
+          label="Input Voltage Unit Select"
+          onChange={onInputVoltageUnitChange}
+        >
+          <MenuItem value={'µV'}>µV</MenuItem>
+          <MenuItem value={'mV'}>mV</MenuItem>
+          <MenuItem value={'V'}>V</MenuItem>
+          <MenuItem value={'kV'}>kV</MenuItem>
+          <MenuItem value={'MV'}>MV</MenuItem>
+        </Select>
+        
+        <OpAmpSchematic 
+          inverting={state.inverting}
+          feedbackResistorValue={`${state.feedbackResistorValue} ${state.feedbackResistorUnit}`}
+          inputResistorValue={`${state.inputResistorValue} ${state.inputResistorUnit}`} 
+          inputVoltageValue={`${state.inputVoltage} ${state.inputVoltageUnit}`}
+          outputVoltageValue={`${state.outputVoltage || 0} V`}
+        />
+        
+        {/* <OpAmpSchematicImg></OpAmpSchematicImg> */}
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/OpAmpSchematic/OpAmpSchematic.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+import invertingAmpSchematic from '../../images/inverting-amplifier.png'
+import nonInvertingAmpSchematic from '../../images/non-inverting-amplifier.png'
+
+const OpAmpSchematicImg = styled.img<{}>`
+  max-width: 80%;
+  height: auto;
+  padding: 0;
+  margin: 0;
+  position: relative
+`;
+
+const InvertingOpAmpInputResistorLbl = styled.p<{}>`
+  position: absolute;
+  top: 58%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+`;
+
+const InvertingOpAmpFeedbackResistorLbl = styled.p<{}>`
+  position: absolute;
+  top: 12%;
+  left: 54%;
+  transform: translate(-50%, -50%);
+`;
+
+const InvertingOpAmpInputVoltageLbl = styled.p<{}>`
+  position: absolute;
+  top: 49%;
+  left: 20%;
+  transform: translate(-50%, -50%);
+`;
+
+const InvertingOpAmpOutputVoltageLbl = styled.p<{}>`
+  position: absolute;
+  top: 24%;
+  right: 20%;
+  transform: translate(-50%, -50%);
+`;
+
+const NonInvertingOpAmpFeedbackResistorLbl = styled.p<{}>`
+  position: absolute;
+  top: 72%;
+  left: 46%;
+  transform: translate(-50%, -50%);
+`;
+
+const NonInvertingOpAmpInputResistorLbl = styled.p<{}>`
+  position: absolute;
+  top: 72%;
+  left: 28%;
+  transform: translate(-50%, -50%);
+`;
+
+const NonInvertingOpAmpInputVoltageLbl = styled.p<{}>`
+  position: absolute;
+  top: 21%;
+  left: 22%;
+  transform: translate(-50%, -50%);
+`;
+
+const NonInvertingOpAmpOutputVoltageLbl = styled.p<{}>`
+  position: absolute;
+  top: 30%;
+  left: 80%;
+  transform: translate(-50%, -50%);
+`;
+
+const Container = styled.div<{}>`
+  position: relative;
+`;
+
+interface Props {
+  inverting: boolean,
+  feedbackResistorValue: string,
+  inputResistorValue: string,
+  inputVoltageValue: string,
+  outputVoltageValue: string
+}
+
+const OpAmpSchematic = (props: Props) => {
+  const { 
+    inverting, 
+    inputResistorValue,
+    inputVoltageValue,
+    feedbackResistorValue, 
+    outputVoltageValue
+  } = props;
+
+  return (
+    <Box sx={{ border: 'solid 10px black', marginTop: '10%' }}>
+      { inverting ? 
+      (
+        <>
+          <Container>
+            <OpAmpSchematicImg src={invertingAmpSchematic}/>
+            <InvertingOpAmpInputResistorLbl>{ inputResistorValue }</InvertingOpAmpInputResistorLbl>
+            <InvertingOpAmpFeedbackResistorLbl>{ feedbackResistorValue }</InvertingOpAmpFeedbackResistorLbl>
+            <InvertingOpAmpInputVoltageLbl>{ inputVoltageValue }</InvertingOpAmpInputVoltageLbl>
+            <InvertingOpAmpOutputVoltageLbl>{ outputVoltageValue }</InvertingOpAmpOutputVoltageLbl>
+          </Container>
+        </>
+      ): (
+        <Container>
+          <OpAmpSchematicImg src={nonInvertingAmpSchematic} />
+          <NonInvertingOpAmpFeedbackResistorLbl>{ feedbackResistorValue }</NonInvertingOpAmpFeedbackResistorLbl>          
+          <NonInvertingOpAmpInputResistorLbl>{ inputResistorValue }</NonInvertingOpAmpInputResistorLbl>          
+          <NonInvertingOpAmpInputVoltageLbl>{ inputVoltageValue }</NonInvertingOpAmpInputVoltageLbl>          
+          <NonInvertingOpAmpOutputVoltageLbl>{ outputVoltageValue }</NonInvertingOpAmpOutputVoltageLbl>
+        </Container>
+      )}
+    </Box>
+  )
+};
+
+export { OpAmpSchematic };
